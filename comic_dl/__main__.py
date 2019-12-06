36a3375f9bf2b4fc3d93cd4dd4fdfd015d3aa097
Enyioma Osondu <o.enyioma@gmail.com>
2019-12-07 00:10:02 +0100

    enc main in a function

--- comic_dl/__main__.py
+++ comic_dl/__main__.py
@@ -3,6 +3,11 @@ import sys
 from commands import CommandUtility
 
 
-argv = sys.argv
-with CommandUtility(argv) as c:
-    c.execute()
+def main():
+    argv = sys.argv
+    with CommandUtility(argv) as c:
+        c.execute()
+
+
+if __name__ == '__main__':
+    main()
