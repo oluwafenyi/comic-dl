5fcac569a2f4fe56ee2b3f8e3b8e16dc534581e3
Enyioma Osondu <o.enyioma@gmail.com>
2019-12-10 00:16:40 +0100

    cosmetic change

--- comic_dl/common/exceptions.py
+++ comic_dl/common/exceptions.py
@@ -3,20 +3,20 @@ class InvalidRangeException(Exception):
     ...
 
 
-class ComicDoesNotExist(Exception):
-    msg = 'The alias specified is not related with any comic in the database,'\
-        ' add comic with comic_dl --add="<query>" to set alias'
-
-    def __init__(self, msg=msg, *args, **kwargs):
-        super().__init__(msg, *args, **kwargs)
-
-
 class PlatformNotSupported(Exception):
     ...
 
 
 class ArgumentNotSpecified(Exception):
     ...
+
+
+class ComicDoesNotExist(Exception):
+    msg = 'The alias specified is not related with any comic in the database,'\
+        ' add comic with comic_dl --add="<query>" to set alias'
+
+    def __init__(self, msg=msg, *args, **kwargs):
+        super().__init__(msg, *args, **kwargs)
 
 
 class AliasNotSpecified(ArgumentNotSpecified):
